aea50099716a9c6cd9b81171dafee7652f568c61
chongwei-fsd <ChongWei@genstudents.org>
2025-02-05 15:56:39 +0800

    update: 1. added menu class methods 2. added GUI

--- Main.java
+++ Main.java
@@ -1,29 +1,51 @@
 package mini_proj;
 
+import mini_proj.GUI.MenuGUI;
+
+import javax.swing.*;
 import java.util.Scanner;
 
 public class Main {
     private Scanner scanner=new Scanner(System.in);
 
+    private Menu menu=new Menu();
+
     private TaskManager taskManager=new TaskManager();
     private EventController eventController=new EventController();
     private DeadlineTaskController deadlineTaskController=new DeadlineTaskController();
     private FloatingTaskController floatingTaskController=new FloatingTaskController();
-    private SearchController searchController=new SearchController();
 
     public static void main(String[] args) {
 //        Main main=new Main();
 //        main.start();
-        new Main().start();
+        new Main().menu();
     }
 
-    public void start(){
+    public void menu(){
+        int input;
+        while (true){
+            menu.gui_cli_Menu();
+            try{
+                input=Integer.parseInt(scanner.nextLine());
+            }catch (NumberFormatException e){
+                System.out.println("invalid. only digit");
+                continue;
+            }
+            switch (input){
+                case 1 -> {
+                    menuGUI();
+                    return;
+                }
+                case 2 -> cliMenu();
+                default -> System.out.println("invalid. only 1-2");
+            }
+        }
+    }
+
+    public void cliMenu(){
         int input;
         while(true) {
-            System.out.println("1. event");
-            System.out.println("2. task with deadline");
-            System.out.println("3. task without deadline (floating task)");
-            System.out.println("4. search event/task");
+            menu.cliMenu();
             try{
                 input=Integer.parseInt(scanner.nextLine());
             }catch(NumberFormatException e){
@@ -34,32 +56,7 @@ public class Main {
                 case 1 -> eventMenu();
                 case 2 -> deadlineTaskMenu();
                 case 3 -> floatingTaskMenu();
-                case 4 -> searchMenu();
-                default -> System.out.println("invalid. only 1-4");
-            }
-        }
-    }
-
-    public void searchMenu() {
-        int choice;
-        while(true){
-            System.out.println("1. search by title");
-            System.out.println("2. search by description");
-            System.out.println("3. search by completion status");
-            System.out.println("4. search events by date range");
-            System.out.println("5. back to main menu");
-            try{
-                choice=Integer.parseInt(scanner.nextLine());
-            }catch(NumberFormatException e){
-                System.out.println("invalid. only digit");
-                continue;
-            }
-            switch (choice){
-                case 1 -> searchController.searchByTitle(taskManager,scanner);
-                case 2 -> searchController.searchByDesc(taskManager,scanner);
-                case 3 -> searchController.searchByCompletion(taskManager,scanner);
-                case 4 -> {return;}
-                default -> System.out.println("invalid. only 1-4");
+                default -> System.out.println("invalid. only 1-3");
             }
         }
     }
@@ -67,11 +64,7 @@ public class Main {
     public void floatingTaskMenu() {
         int floatingTaskChoice;
         while(true){
-            System.out.println("1. add floating task");
-            System.out.println("2. display floating task");
-            System.out.println("3. edit floating task");
-            System.out.println("4. delete floating task");
-            System.out.println("5. back to main menu");
+            menu.floatingTaskMenu();
             try{
                 floatingTaskChoice=Integer.parseInt(scanner.nextLine());
             }catch(NumberFormatException e){
@@ -83,7 +76,8 @@ public class Main {
                 case 2 -> floatingTaskController.displayTask(taskManager);
                 case 3 -> floatingTaskController.editTask(taskManager,scanner);
                 case 4 -> floatingTaskController.deleteTask(taskManager,scanner);
-                case 5 -> {return;}
+                case 5 -> floatingTaskController.searchTask(taskManager,scanner);
+                case 6 -> {return;}
                 default -> System.out.println("invalid. only 1-5");
             }
         }
@@ -93,11 +87,7 @@ public class Main {
     public void eventMenu(){
         int eventChoice;
         while(true){
-            System.out.println("1. add event");
-            System.out.println("2. display event");
-            System.out.println("3. edit event");
-            System.out.println("4. delete event");
-            System.out.println("5. back to main menu");
+                menu.eventMenu();
             try{
                 eventChoice =Integer.parseInt(scanner.nextLine());
             }catch (NumberFormatException e){
@@ -109,7 +99,8 @@ public class Main {
                 case 2 -> eventController.displayEvent(taskManager);
                 case 3 -> eventController.editEvent(taskManager,scanner);
                 case 4 -> eventController.deleteEvent(taskManager,scanner);
-                case 5 -> {return;}
+                case 5 -> eventController.searchEvent(taskManager,scanner);
+                case 6 -> {return;}
                 default -> System.out.println("invalid. only 1-5");
             }
         }
@@ -118,11 +109,7 @@ public class Main {
     public void deadlineTaskMenu(){
         int deadlineTaskChoice;
         while(true){
-            System.out.println("1. add deadline task");
-            System.out.println("2. display deadline task");
-            System.out.println("3. edit deadline task");
-            System.out.println("4. delete deadline task");
-            System.out.println("5. back to main menu");
+                menu.deadlineTaskMenu();
             try{
                 deadlineTaskChoice =Integer.parseInt(scanner.nextLine());
             }catch (NumberFormatException e){
@@ -134,10 +121,19 @@ public class Main {
                 case 2 -> deadlineTaskController.displayDeadlineTask(taskManager);
                 case 3 -> deadlineTaskController.editDeadlineTask(taskManager,scanner);
                 case 4 -> deadlineTaskController.deleteDeadlineTask(taskManager,scanner);
-                case 5 -> {return;}
+                case 5 -> deadlineTaskController.searchDeadlineTask(taskManager,scanner);
+                case 6 -> {return;}
                 default -> System.out.println("invalid. only 1-4");
             }
         }
+    }
+
+    public void menuGUI(){
+        SwingUtilities.invokeLater(()->{
+            MenuGUI menuGUI=new MenuGUI();
+            menuGUI.setVisible(true);
+        });
+
     }
 
 
