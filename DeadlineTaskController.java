33a9c79db28f9dfded4a124b9b19310ea2d5fc4e
chongwei-fsd <ChongWei@genstudents.org>
2025-02-05 15:53:55 +0800

    update: 1. style formatting 2. added searchDeadlineTask method

--- DeadlineTaskController.java
+++ DeadlineTaskController.java
@@ -1,9 +1,11 @@
 package mini_proj;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class DeadlineTaskController {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
     PromptCheck promptCheck = new PromptCheck();
     Menu menu = new Menu();
@@ -16,7 +18,7 @@ public class DeadlineTaskController {
 
         DeadlineTask deadlineTask = new DeadlineTask(taskTitle, taskDesc, false, deadline);
         taskManager.addTask(deadlineTask);
-        System.out.println(taskTitle+" added successfully");
+        System.out.println(taskTitle+" added successfully\n");
     }
 
     public void displayDeadlineTask(TaskManager taskManager) {
@@ -27,8 +29,7 @@ public class DeadlineTaskController {
                 flag = true;
                 System.out.println("title: " + t.getTitle());
                 System.out.println("description: " + t.getDescription());
-//                System.out.println("complete: " + (t.isComplete() ? "completed" : "not complete"));
-                System.out.println("complete: " + t.isComplete());
+                System.out.println("complete: " + (t.isComplete()?"Completed":"Not complete"));
                 System.out.println("deadline: " + ((DeadlineTask) t).getDeadline()+"\n");
             }
         }
@@ -43,7 +44,7 @@ public class DeadlineTaskController {
             String taskTitle = promptCheck.getStringInput(scanner, "enter deadline task title:");
             DeadlineTask deadlineTask = findDeadlineTaskTitle(taskManager, taskTitle);
             if (deadlineTask != null) {
-                System.out.println("deadline task title found - " + deadlineTask.getTitle());
+                System.out.println("deadline task title found - " + taskTitle+"\n");
                 do {
                     menu.editDeadlineTaskMenu(deadlineTask.getTitle());
                     int choice;
@@ -63,7 +64,7 @@ public class DeadlineTaskController {
                     }
                 } while (true);
             }
-            if (deadlineTask == null) System.out.println(taskTitle + " not found");
+            if (deadlineTask == null) System.out.println(taskTitle + " not found\n");
 
         } while (true);
     }
@@ -72,25 +73,28 @@ public class DeadlineTaskController {
         System.out.println("********* edit deadline task completion *********");
         boolean taskComplete = promptCheck.setComplete(scanner, "is "+taskTitle+" completed? (Y/N)");
         deadlineTask.setComplete(taskComplete);
-        System.out.println(deadlineTask.getTitle()+" set to "+(taskComplete?"complete":"not complete"));
+        System.out.println(deadlineTask.getTitle()+" set to "+(taskComplete?"complete\n":"not complete\n"));
     }
 
     public void editTaskDeadline(Scanner scanner, String taskTitle, DeadlineTask deadlineTask) {
         System.out.println("********* edit deadline task deadline *********");
         LocalDateTime newDeadline=promptCheck.getDateTimeInput(scanner,"enter "+taskTitle+"'s new deadline");
         deadlineTask.setDeadline(newDeadline);
+        System.out.println(taskTitle+" updated\n");
     }
 
     public void editTaskDesc(Scanner scanner, String taskTitle,DeadlineTask deadlineTask) {
         System.out.println("********* edit deadline task description *********");
         String newDeadlineTaskDesc=promptCheck.getStringInput(scanner,"enter "+taskTitle+"'s new description");
         deadlineTask.setDescription(newDeadlineTaskDesc);
+        System.out.println(taskTitle+" updated\n");
     }
 
     public void editTaskTitle(Scanner scanner, String taskTitle,DeadlineTask deadlineTask) {
         System.out.println("********* edit deadline task title *********");
         String newTaskTitle = promptCheck.getStringInput(scanner, "enter "+taskTitle+"'s new title");
         deadlineTask.setTitle(newTaskTitle);
+        System.out.println(newTaskTitle+" updated\n");
     }
 
     public DeadlineTask findDeadlineTaskTitle(TaskManager taskManager, String taskTitle) {
@@ -107,12 +111,37 @@ public class DeadlineTaskController {
         while(true){
             String taskTitle=promptCheck.getStringInput(scanner,"enter deadline task title you wish to delete");
             DeadlineTask deadlineTask=findDeadlineTaskTitle(taskManager,taskTitle);
-            if(deadlineTask==null) System.out.println(taskTitle+" not found");
+            if(deadlineTask==null) System.out.println(taskTitle+" not found\n");
             if(deadlineTask!=null) {
                 taskManager.deleteTask(deadlineTask);
-                System.out.println(taskTitle+" has been deleted");
+                System.out.println(taskTitle+" has been deleted\n");
                 return;
             }
         }
     }
+
+    public void searchDeadlineTask(TaskManager taskManager, Scanner scanner) {
+        boolean flag=false;
+        String searchText=promptCheck.getStringInput(scanner,"search for deadline task (title/description)");
+        System.out.println("***************** deadline task **********************");
+        for(Task t:taskManager.getTasks()){
+            if(t instanceof DeadlineTask){
+                if(t instanceof DeadlineTask){
+                    if(t.getTitle().contains(searchText) || t.getDescription().contains(searchText)){
+                        flag=true;
+                        System.out.println("title: "+t.getTitle());
+                        System.out.println("description: "+t.getDescription());
+                        System.out.println("deadline: "+((DeadlineTask) t).getDeadline());
+                        System.out.println("completion: "+(t.isComplete()?"Completed\n":"Not complete\n"));
+                    }
+                }
+            }
+        }
+        if(!flag){
+            System.out.println(searchText+" not found\n");
+        }
+    }
+
+
+
 }
